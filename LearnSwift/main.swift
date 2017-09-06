3587455fcc7dc18b0dbbaad0bf5bdc4ff0ef9ac3
Lydia Krasucki <lydiakrasucki@mwc-078156.dhcp.missouri.edu>
2017-09-06 15:15:47 -0500

    Initial Commit

---
+++ LearnSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LearnSwift
+//
+//  Created by Lydia Krasucki on 9/6/17.
+//  Copyright © 2017 Lydia Krasucki. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
