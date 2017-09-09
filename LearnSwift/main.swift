0c3bb9d28b1bce0dcbc2133a11c287ea85f2c644
Lydia Krasucki <lydiakrasucki@Lydias-MacBook-Pro.local>
2017-09-08 21:39:26 -0500

    First Commit

--- LearnSwift/main.swift
+++ LearnSwift/main.swift
@@ -8,5 +8,108 @@
 
 import Foundation
 
-print("Hello, World!")
+let sample1: UInt8
+    sample1 = 0x3A
 
+var sample2: UInt8
+    sample2 = 58
+
+var heartRate: Int
+    heartRate = 85
+
+var deposits: Double
+    deposits = 135002796
+
+let acceleration: Float
+    acceleration = 9.800
+
+var mass: Float
+    mass = 14.6
+
+var distance: Double
+    distance = 129.763001
+
+var lost: Bool
+    lost = true
+
+var expensive: Bool
+    expensive = true
+
+var choice: Int
+    choice = 2
+
+let integral: Character
+    integral = "\u{222B}"
+
+let greeting: String
+    greeting = "Hello"
+
+var name: String
+    name = "Karen"
+
+if sample1 == sample2{
+print("The samples are equal")
+}
+
+if sample1 != sample2{
+print("The samples are not equal.")
+}
+
+if heartRate >= 40 && heartRate <= 80{
+print("Heart rate is normal.")
+}
+
+else{
+print("Heart rate is not normal.")
+}
+
+if deposits >= 100000000{
+print("You are exceedingly wealthy.")
+}
+
+else{
+print("Sorry you are so poor.")
+}
+
+var force: Float
+    force = mass * acceleration
+
+    print("force = \(force)")
+
+if lost && expensive == true{
+    print("I am really sorry! I will get the manager.")
+}
+
+else{
+    print("Here is coupon for 10% off.")
+}
+
+switch choice{
+    
+case 1:
+    print("You chose 1.")
+case 2:
+    print("You chose 2.")
+case 3:
+    print("You chose 3.")
+default:
+    print("You made an unknown choice.")
+}
+
+print("\(integral) is an integral")
+
+var i : Int
+
+for i in 5...10{
+    print("i = \(i)")
+}
+
+var age : Int
+age = 0
+
+while age < 6{
+    print("age = \(age)")
+    age += 1
+}
+
+print("\(greeting) \(name)")
